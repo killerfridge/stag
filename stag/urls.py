0e2a86f311b0718e3652f7fbf4c5530331bb460d
Dan Russell-Brain <dan.russell-brain@philips.com>
2020-02-09 15:39:48 +0000

    Commited main changes including adding the css for animations

--- stag/urls.py
+++ stag/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views import QuestionListView
+from .views import QuestionListView, HomeView
+
+app_name = 'stag'
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('questions/', QuestionListView.as_view(), name='questions')
 ]
--- stag/views.py
+++ stag/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import Question
 # Create your views here.
 
 
 class QuestionListView(ListView):
     model = Question
+    template_name = 'stag/question_list.html'
+
+
+class HomeView(TemplateView):
+    template_name = 'stag/home.html'
